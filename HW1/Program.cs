20c204a26d00968138c3a2e1ce94a9ef6b581c0f
Kirill <Nightmare643@outlook.com>
2023-09-28 19:40:44 +0300

    Перове ДЗ

---
+++ HW1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int num(string mes)
+        {
+            Console.WriteLine(mes);
+            string i = Console.ReadLine();
+            int result = int.Parse(i);
+            return result;
+        }
+        int number = num("Введите трехзначное число > ");
+        if (number < 100 || number >= 1000)
+        {
+            Console.WriteLine("Не трехзначное, повторите попытку");
+            return;
+        }
+        Console.WriteLine($"Это трехзначное число {number} ");
+        int Second = number / 10 % 10;
+        Console.WriteLine($"Второая {Second} ");
+
+        
+
+    }
+}
